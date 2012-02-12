9eb326298e94918177b55e568b93dd85e2e39beb
Michael Enger <mike@thelonelycoder.com>
2012-02-12 20:11:24 +0100

    Bootstrap the class and license information

---
+++ ScrollLockView.m
@@ -0,0 +1,31 @@
+//
+//  ScrollLockView.m
+//  ScrollLockView - A view that attaches itself to a UIScrollView and locks the
+//                   scroll when visible.
+//                   Inspired by: https://github.com/iStopped/PullToRefreshView
+//
+//  Created by Michael Enger on 2/12/12.
+//  Copyright (c) 2012 The Lonely Coder. All rights reserved.
+//
+//  This software is provided 'as-is', without any express or implied warranty.
+//  In no event will the authors be held liable for any damages arising from the
+//  use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//   * The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software in a
+//     product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//   * Altered source versions must be plainly marked as such, and must not be
+//     misrepresented as being the original software.
+//   * This notice may not be removed or altered from any source distribution.
+//
+
+#import "ScrollLockView.h"
+
+@implementation ScrollLockView
+
+@end
